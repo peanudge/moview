c6aaf23a5854412d24680f682fd3fb8ccb37ac53
peanut-lover <jihoson94@gmail.com>
2021-10-28 15:13:30 +0900

    App Loading

--- App.js
+++ App.js
@@ -1,5 +1,25 @@
-import React from "react";
+import AppLoading from "expo-app-loading";
+import React, { useState } from "react";
+import { Text } from "react-native";
 
 export default function App() {
-  return null;
+  const [ready, setReady] = useState(false);
+
+  const onFinish = () => setReady(true);
+
+  const startLoading = async () => {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+  };
+
+  if (!ready) {
+    return (
+      <AppLoading
+        startAsync={startLoading}
+        onFinish={onFinish}
+        onError={console.error}
+      />
+    );
+  }
+
+  return <Text>We are done loading!</Text>;
 }
